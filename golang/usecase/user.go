60dcf27606208d46fcf35a8d8140ef741e0a1275
AokabiC <syht0312@gmail.com>
2021-07-15 18:36:20 +0900

    use interface in usecase

--- golang/usecase/user.go
+++ golang/usecase/user.go
@@ -2,26 +2,47 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 	"go-clean/domain"
 )
+
+type UserUsecase interface {
+	// IDに一致するUserを返す。見つからない場合はerrを返す。
+	GetByID(ctx context.Context, id int) (*domain.User, error)
+	// 与えられた名前でUserを作成し、作成されたUserを返す。
+	Create(ctx context.Context, username string) (*domain.User, error)
+}
 
 type UserInteractor struct {
 	UserRepository domain.UserRepository
 }
 
-func (interactor *UserInteractor) GetById(ctx context.Context, id int) (*domain.User, error) {
-	user, err := interactor.UserRepository.GetByID(ctx, id)
+var _ UserUsecase = (*UserInteractor)(nil)
+
+func (interactor *UserInteractor) GetByID(ctx context.Context, id int) (*domain.User, error) {
+	user, err := interactor.UserRepository.GetByID(ctx, domain.UserID(id))
 	return user, err
 }
 
-func (interactor *UserInteractor) Create(ctx context.Context, name string) (*domain.User, error) {
-	user, err := domain.NewUser(name)
+func (interactor *UserInteractor) Create(ctx context.Context, username string) (*domain.User, error) {
+	newUsername, err := domain.NewUsername(username)
+	if err != nil {
+		return nil, ErrInvalidUserCreateRequest(err)
+	}
+
+	userWithoutID, err := domain.NewUser(-1, newUsername)
 	if err != nil {
 		return nil, err
 	}
-	err = interactor.UserRepository.Create(ctx, user)
+
+	user, err := interactor.UserRepository.Create(ctx, *userWithoutID)
 	if err != nil {
 		return nil, err
 	}
+
 	return user, nil
+}
+
+func ErrInvalidUserCreateRequest(err error) error {
+	return fmt.Errorf("invalid user create request: %w", err)
 }
